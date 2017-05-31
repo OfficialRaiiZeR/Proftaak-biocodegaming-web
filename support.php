993714a9d42621a3a394d876b1291805e3ae7785
Alex Mares <alexmares0304@gmail.com>
2017-05-31 11:56:52 +0200

    updated documents and added support form

--- support.php
+++ support.php
@@ -52,17 +52,72 @@ session_start();
       </div>
     </nav>
 
-    <div class="jumbotron">
+    <!-- <div class="jumbotron">
       <div class="container">
         <h1>Welcome to biocodegaming</h1>
         <p class="maintext">This is BioCodeGaming, the Number 1 platfrom for all your community stuff and gaming servers. We also have a installable application from this website where you will have the same functionality</p>
         <p><a class="btn btn-primary btn-lg" href="application.php" role="button">Learn more &raquo;</a></p>
       </div>
     </div>
-
+ -->
     <div class="container">
-      <h1>Latest news and releases</h1>
+      <?php
+        if (isset($_REQUEST['email']))  
+        {
+          //Email information
+          $admin_email = "alexmares0304@gmail.com";
+          $email = mysql_real_escape_string($_REQUEST['email']);
+          $subject = "BioCodeGaming Support mail";
+          $comment = mysql_real_escape_string($_REQUEST['Message']);
+          $firstName = mysql_real_escape_string($_REQUEST['firstName']);
+          $insertion = mysql_real_escape_string($_REQUEST['insertion']);
+          $lastName = mysql_real_escape_string($_REQUEST['lastName']);
 
+          //send email
+          mail($admin_email, "$subject", $comment, "From:" . $email, $firstName . $insertion . $lastName);
+            
+          //Email response
+          echo "Thank you for contacting us! <br /> We will resolve your problem as soon as possible.";
+        }
+          
+        //if "email" variable is not filled out, display the form
+        else {
+      ?>
+      
+      <form action="support.php" method="POST">
+        <ul id="ulForm">
+          <div id="ContactTitle">Contact</div>
+          <li>
+            <label>Firstname:</label>
+            <input type="text" name="firstName" value="">
+          </li>
+          <li>
+            <label>Insertion:</label>
+            <input type="text" name="insertion" value="">
+          </li>
+          <li>
+            <label>Lastname:</label>
+            <input type="text" name="lastName" value="">
+          </li>
+          <li>
+            <label>Email address:</label>
+            <input type="email" name="email" value="">
+          </li>
+          <li>
+            <label>Explain Your problem:</label>
+            <textarea id="messageBox" type="text" name="Message" value=""></textarea>
+          </li>
+          <li>
+            <div id="submitButton">
+              <input type="submit" name="submit" value="Send">
+            </div>
+          </li>
+        </ul>
+      </form>
+
+          <?php
+            }
+          ?>
       <hr>
 
       <footer>
@@ -70,14 +125,10 @@ session_start();
       </footer>
     </div> 
 
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
     <script src="../../dist/js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+
     <script src="js/ie10-viewport-bug-workaround.js"></script>
     <script type="text/javascript" src="js/main.js"></script>
   </body>
